9aaaec5986de465d24c362f39dfa16f1bd4d5b55
khalil <khalil.pan2@gmail.com>
2020-03-12 21:13:38 -0300

    application working correctly

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,43 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text("8Ball"),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: MyApp(),
+        ),
       ),
     );
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Row(
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            child: Image.asset('images/ball$ballNumber.png'),
+            onPressed: () {
+              setState(() {
+                ballNumber = Random().nextInt(4) + 1;
+              });
+            },
+          ),
+        ),
+      ],
+    ));
+  }
+}
